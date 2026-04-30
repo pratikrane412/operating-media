0460a347a5bf03d40ee37c2fc1658e3b182cb653
RiteshDev <tech@ibraine.com>
2026-04-30 17:39:59 +0530

    hir from us page changes

--- src/pages/HireFromUsPage/HireFromUsPage.jsx
+++ src/pages/HireFromUsPage/HireFromUsPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import HireFromUsSection from "../../components/HireFromUsPage/HireFromUsSection/HireFromUsSection";
+import Breadcum from "../../components/HireFromUsPage/HireFromUsSection/Breadcum";
 
 
 const HireFromUsPage = () => {
   return (
     <main className="bg-white min-h-screen">
       {/* 1. Hero Section */}
+      <Breadcum />
       <HireFromUsSection />
-     
+    
       
 
       
