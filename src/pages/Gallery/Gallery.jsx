64a33687d8220c37f2130fabc440c53a45c44f8e
RiteshDev <tech@ibraine.com>
2026-04-30 17:15:44 +0530

    gallery page changes

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import Breadcum from "../../components/Gallery/Breadcum.jsx";
 import GallerySection from "../../components/Gallery/gallerysection.jsx";
+import InstitutionCampus from '../../components/MastersProgramPage/InstitutionCampus/InstitutionCampus.jsx';
+
 
 const gallerypage = () => {
   return (
     <main>
       <Breadcum />
       <GallerySection />
+             <InstitutionCampus/>
       
     </main>
   );
